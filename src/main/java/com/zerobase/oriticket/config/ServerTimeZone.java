2ae8f12bdf9afd084df6a95eab2f9fa9ce0191b9
Stone <kangrack333@gmail.com>
2023-12-02 19:40:58 +0900

    test : 스프링 실행 테스트

--- src/main/java/com/zerobase/oriticket/config/ServerTimeZone.java
+++ src/main/java/com/zerobase/oriticket/config/ServerTimeZone.java
@@ -1,14 +1,14 @@
-package com.zerobase.oriticket.config;
-
-import jakarta.annotation.PostConstruct;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.TimeZone;
-
-@Configuration
-public class ServerTimeZone {
-    @PostConstruct
-    public void setServerTimeZone() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-    }
-}
+//package com.zerobase.oriticket.config;
+//
+//import jakarta.annotation.PostConstruct;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.TimeZone;
+//
+//@Configuration
+//public class ServerTimeZone {
+//    @PostConstruct
+//    public void setServerTimeZone() {
+//        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+//    }
+//}
