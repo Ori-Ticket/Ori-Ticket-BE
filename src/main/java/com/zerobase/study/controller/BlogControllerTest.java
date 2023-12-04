ff6d7466dfd2ab600d40ec68a3450f80d57257ee
Stone <kangrack333@gmail.com>
2023-12-04 17:05:39 +0900

    feat : 카카오 API 로그인 구현

--- src/main/java/com/zerobase/study/controller/BlogControllerTest.java
+++ src/main/java/com/zerobase/study/controller/BlogControllerTest.java
@@ -1,16 +1,15 @@
 package com.zerobase.study.controller;
 
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BlogControllerTest {
 
-    @GetMapping("test/hello")
-    public String hello() {
-        return "<h1>hello spring boot</h1>";
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "oriticket/user/loginPage";
     }
 
 }
