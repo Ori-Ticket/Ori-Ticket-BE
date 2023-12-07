d1b00862d1219ff05854eaa476d393b3e0524879
hann0079 <haeun8701@gmail.com>
2023-12-07 15:36:57 +0900

    Refactor: import 정리

--- src/main/java/com/zerobase/oriticket/post/repository/SportsRepository.java
+++ src/main/java/com/zerobase/oriticket/post/repository/SportsRepository.java
@@ -1,7 +1,6 @@
 package com.zerobase.oriticket.post.repository;
 
 import com.zerobase.oriticket.post.entity.Sports;
-import com.zerobase.oriticket.post.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
