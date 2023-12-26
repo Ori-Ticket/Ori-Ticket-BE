5bfc6ddc6cb1f81dd98a6521a27abe13e702f360
KSY <ggomaDVLP@gmail.com>
2023-12-26 20:20:30 +0900

    refactor: 안쓰는 import 제거

--- src/main/java/com/zerobase/oriticket/OriticketApplication.java
+++ src/main/java/com/zerobase/oriticket/OriticketApplication.java
@@ -2,7 +2,6 @@ package com.zerobase.oriticket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication //(exclude = DataSourceAutoConfiguration.class)
 public class OriticketApplication {
